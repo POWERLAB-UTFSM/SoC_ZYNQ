0a74c599cf3a285254be96ba11c5b64f41b67ddf
Alan Wilson <alan.wilson@usm.cl>
2023-03-16 01:22:31 -0300

    new vitis project generator

--- Vitis/projects/test_1.tcl
+++ Vitis/projects/test_1.tcl
@@ -3,3 +3,4 @@ cd ./workspace
 app create -name test_1 -hw ../../../Vivado/xsa_hardware/pwm_1_wrapper.xsa -os standalone -proc ps7_cortexa9_0 -template {Empty Application}
 importsources -name test_1 -path ../../src/helloworld.c
 app build -name test_1
+closehw ../../../Vivado/xsa_hardware/pwm_1_wrapper.xsa
