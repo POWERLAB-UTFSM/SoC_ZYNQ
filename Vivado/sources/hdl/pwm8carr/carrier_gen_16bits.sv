491dd6bd61a129980bbf9948e46d2a2f3c5a1ae6
Alan Wilson <alan.wilson@usm.cl>
2023-03-24 00:44:00 -0300

    fsm cpwm8c ok

--- Vivado/sources/hdl/pwm8carr/carrier_gen_16bits.sv
+++ Vivado/sources/hdl/pwm8carr/carrier_gen_16bits.sv
@@ -32,135 +32,191 @@ module carrier_gen_16bits(
     output logic [`PWMCOUNT_WIDTH-1:0] carrier
     //output logic maskevent
     );
-    
-    enum bit {UP, DOWN} state_carrier;
-    logic carrier_mask;
-    logic [`PWMCOUNT_WIDTH-1:0] init_carr_buff;
-    logic init_carr_change;
-    wire init_carr_comp;
-    wire pwmcarr_onoff;
-    
-    assign pwmcarr_onoff = ((pwm_onoff==PWM_OFF)) | ((count_mode==NO_COUNT) | (period=='d0));
-    
-    //always_comb begin
-    assign  init_carr_comp = init_carr_buff!=init_carr;
-    //end 
-    always_ff @(posedge reset or posedge pwmcarr_onoff) begin
-        if(reset==1'b1) begin
-            carrier_mask <= 1'b0;
-            init_carr_change <= 0;
-            init_carr_buff <= 1'b0;
-        end
-    end
-    
-    typedef enum logic [2:0] {S_RESET0,S_UP,S_DOWN,S_RESETP,S_STOP} _state_counter;
-    _state_counter counter_state_k0;
-    _state_counter counter_state_k1;
+
+    //------------------------------------
+    //State definition
+    typedef enum logic [2:0] {S_RESET0,S_UP,S_DOWN,S_RESETP,S_STOP,S_NEWCARR} _state_counter;
+    _state_counter counter_state;
+    _state_counter counter_state_next;
+    logic stop_status;
+    logic [`PWMCOUNT_WIDTH-1:0] init_carr_buffer;
+    logic new_carrier;
     
     always_comb begin
-        counter_state_k1=counter_state_k0;
-        case(counter_state_k0) 
+        if(count_mode==NO_COUNT || period == 0 || pwm_onoff == PWM_OFF) begin
+            stop_status=1;
+        end
+        else begin
+            stop_status=0;
+        end
+    end
+    
+    //always_comb begin
+    always_ff @(posedge clk) begin
+        if(init_carr !=init_carr_buffer) begin
+            new_carrier=1;
+        end
+    end
+    
+    //always_comb begin
+    always_ff @(posedge clk) begin
+    //------------------------------------
+    //Conditional state transition
+        //counter_state_next=counter_state;
+        case(counter_state) 
             S_RESET0: begin
-                if(count_mode==COUNT_DOWN) begin
-                    carrier = 0;
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
                 end
-                else if(count_mode==COUNT_UP || count_mode==COUNT_UPDOWN);
+                else
+                if (new_carrier==1) begin
+                    counter_state_next = S_NEWCARR;
+                end
+                else  
+                if(count_mode==COUNT_DOWN) begin
+                    counter_state_next = S_RESETP;
+                end
+                else
+                if((count_mode==COUNT_UP || count_mode==COUNT_UPDOWN) && carrier < period) begin
+                    counter_state_next = S_UP;
+                end
+                
             end
-        endcase      
+            S_RESETP: begin
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
+                end
+                else
+                if (new_carrier==1) begin
+                    counter_state_next = S_NEWCARR;
+                end
+                else
+                if(count_mode==COUNT_DOWN && carrier > 0) begin
+                    counter_state_next = S_DOWN;
+                end
+                else 
+                if(count_mode==COUNT_UP || count_mode==COUNT_UPDOWN) begin
+                    counter_state_next = S_UP;
+                end
+                
+            end
+            S_UP: begin
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
+                end
+                else
+                if (new_carrier==1) begin
+                    counter_state_next = S_NEWCARR;
+                end
+                else 
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_DOWN) && carrier >= period) begin
+                    counter_state_next = S_DOWN;
+                end
+                else 
+                if(count_mode==COUNT_UP && carrier >= period) begin
+                    counter_state_next = S_RESET0;
+                end
+                
+            end
+            S_DOWN: begin
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
+                end
+                else
+                if (new_carrier==1) begin
+                    counter_state_next = S_NEWCARR;
+                end
+                else 
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_UP) && carrier <=0) begin
+                    counter_state_next = S_UP;
+                end
+                else 
+                if(count_mode==COUNT_DOWN && carrier <= 0) begin
+                    counter_state_next = S_RESETP;
+                end
+                
+            end
+            S_STOP: begin
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
+                end
+                else
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_UP) && carrier < period) begin
+                    counter_state_next = S_UP;
+                end
+                else 
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_DOWN ) && carrier > 0) begin
+                    counter_state_next = S_DOWN;
+                end
+                else 
+                if(carrier >= period && stop_status==0) begin
+                    counter_state_next = S_RESETP;
+                end
+                else 
+                if(carrier <= 0 && stop_status==0) begin
+                    counter_state_next = S_RESET0;
+                end
+            end
+            S_NEWCARR: begin
+                if(stop_status==1) begin
+                    counter_state_next = S_STOP;
+                end
+                else 
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_UP) && carrier < period) begin
+                    counter_state_next = S_UP;
+                end
+                else 
+                if((count_mode==COUNT_UPDOWN || count_mode==COUNT_DOWN ) && carrier > 0) begin
+                    counter_state_next = S_DOWN;
+                end
+                else 
+                if(carrier >= period && stop_status==0) begin
+                    counter_state_next = S_RESETP;
+                end
+                else 
+                if(carrier <= 0 && stop_status==0) begin
+                    counter_state_next = S_RESET0;
+                end
+            end
+        endcase
+            
     end
     
-    
-    /*always_comb begin
-    //always_ff @(posedge clk or posedge reset) begin
-        if(reset==1'b1) begin
-            carrier_mask <= 1'b0;
-            init_carr_change <= 0;
-            init_carr_buff <= 1'b0;
-        end
-        
-        else if (pwmcarr_onoff) begin
-            carrier_mask <= 1'b0;
-        end
-        
-        else if((init_carr_comp)) begin
-            carrier_mask <= 1'b0;
-            init_carr_change <= 1;
-            init_carr_buff <= init_carr;
-        end
-        
-        else begin
-            carrier_mask <= 1'b1;
-            init_carr_change <= 0;
-        end
-    end*/
-    
     always_ff @(posedge clk or posedge reset) begin
-        if(init_carr_comp) begin
-            init_carr_change <= 1;
-            init_carr_buff <= init_carr;
+        if (reset) begin
+            counter_state <= S_NEWCARR;
+            init_carr_buffer <=0;
+        end
+        else begin
+            counter_state <= counter_state_next;
+            init_carr_buffer <=init_carr;
         end
     end
     
-    /*always_ff @(posedge clk or posedge reset) begin
-        if(init_carr_comp || reset) begin
-            init_carr_change <= 1;
-            init_carr_buff <= init_carr;
-        end
-        else begin
-            init_carr_change <= 0;
-        end
+    always_ff @(posedge clk) begin
+        //------------------------------------
+        //State actions
+        case(counter_state_next) 
+            S_RESET0: begin
+                carrier = 0;
+            end
+            S_RESETP: begin
+                carrier = (period) ;
+            end
+            S_UP: begin
+                carrier = (carrier +1) ;
+            end
+            S_DOWN: begin
+                carrier = (carrier -1) ;
+            end
+            S_STOP: begin
+                carrier = 0;
+            end
+            S_NEWCARR: begin
+                carrier = init_carr;
+                new_carrier=0;
+            end
+        endcase
     end
-    
-    always_ff @(posedge clk or posedge reset) begin
-        if(reset || pwmcarr_onoff || init_carr_comp) begin
-            carrier_mask <= 1'b0;
-        end
-        
-        else begin
-            carrier_mask <= 1'b1;
-        end
-    end*/
-    
-    always_ff @(posedge clk or posedge reset) begin
-        if(reset) begin
-            carrier <=0;
-            state_carrier<=UP;
-        end
-        /*                                                                                       else if(carrier_mask==1'b0) begin
-            if((init_carr_change)) begin
-                carrier <= init_carr;
-            end
-            else begin
-                carrier <= 0;
-            end
-            //state_carrier <= UP;
-        end*/
-        
-        else if(carrier_mask==1'b1 && carrier<period && state_carrier==UP) begin
-            carrier <= carrier + 'd1;
-        end
-        else if(carrier_mask==1'b1 && carrier>0 && state_carrier==DOWN) begin
-            carrier <= carrier - 'd1;
-        end
-        
-        else if(carrier_mask==1'b1 && carrier>=period) begin
-            if(count_mode==COUNT_UP) begin
-                carrier <= 0;
-                //state_carrier<=UP;
-            end
-            else if(count_mode==COUNT_DOWN || count_mode==COUNT_UPDOWN) begin
-                state_carrier<=DOWN;
-            end
-        end
-        else if(carrier_mask==1'b1 && carrier<=0) begin
-            if(count_mode==COUNT_DOWN) begin
-                carrier <= period;
-                //state_carrier<=DOWN;
-            end
-            else if(count_mode==COUNT_UP || count_mode==COUNT_UPDOWN) begin
-                state_carrier<=UP;
-            end
-        end
-    end 
     
 endmodule
